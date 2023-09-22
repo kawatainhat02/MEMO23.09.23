94c0305235033fc7663ba861d7e8b238fa32fbb9
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-09-23 02:57:35 +0900

    Update Function.js

--- Function.js
+++ Function.js
@@ -1,17 +1,24 @@
-let greeting = function(){   
 
-
- console.log('Hello');   
-
-
-};    
-
-
-greeting(); 
-
-
-Output: 
-
-
-Hello
-
+console.log()
+console.count()
+print()
+callback()
+function()
+recurse()
+setTimeout()
+setInterval()
+alert()
+remove()
+import()
+document.write
+bind()
+Math.min()
+Math.max()
+Math.floor()
+prompt()
+Number()
+then()
+every()
+submit()
+greet()
+require()
